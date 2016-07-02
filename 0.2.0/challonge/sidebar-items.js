65e458a76c436d06aa726ff3bc4ae7e416389ae0
Victor Polevoy <maintainer@vpolevoy.com>
2016-07-02 12:21:53 +0300

    added 0.2.0 docs (early)

---
+++ 0.2.0/challonge/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["error","Challonge REST API error type."],["matches","Challonge Match type."],["participants","Challonge Participant type."],["tournament","Challonge Tournament type."]],"struct":[["Challonge","Client for the Challonge REST API."]]});
